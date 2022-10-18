d4f9e3c3400355610dd1e54b79ee8215117ffbce
Morgan <proger20000@gmail.com>
2022-10-18 18:26:14 +0600

    1.Переменные

--- src/main/java/ru/job4j/calculate/Var.java
+++ src/main/java/ru/job4j/calculate/Var.java
@@ -4,15 +4,15 @@ public class Var {
     public static void main(String[] args) {
         byte  age = 18;
         System.out.println(age);
-        char gender = 'W';
+        char gender =  'W';
         System.out.println(gender);
         float growth = 188.5F;
         System.out.println(growth);
         short amount = 23032;
         System.out.println(amount);
-        double koe = 0.001529f;
+        double koe = 0.001529d;
         System.out.println(koe);
-        double distance = 384400;
+        double distance = 384400d;
         System.out.println(distance);
         boolean  permits = true;
         System.out.println(permits);
