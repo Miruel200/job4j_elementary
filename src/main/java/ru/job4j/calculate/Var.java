31c61d792721bb6c0d906e80fac82a3ad274dcff
Morgan <proger20000@gmail.com>
2022-10-18 17:47:23 +0600

    1. Определите тип данных для переменных.

---
+++ src/main/java/ru/job4j/calculate/Var.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculate;
+
+public class Var {
+    public static void main(String[] args) {
+        byte  age = 18;
+        System.out.println(age);
+        char gender = 'W';
+        System.out.println(gender);
+        float growth = 188.5F;
+        System.out.println(growth);
+        short amount = 23032;
+        System.out.println(amount);
+        double koe = 0.001529f;
+        System.out.println(koe);
+        double distance = 384400;
+        System.out.println(distance);
+        boolean  permits = true;`
+        System.out.println(permits);
+        byte size = 77;
+        System.out.println(size);
+    }
+}
---
+++ src/main/java/ru/job4j/calculate/Variables.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte  age = 26;
+        System.out.println(age);
+        char gender = 'M';
+        System.out.println(gender);
+        float growth = 182.5F;
+        System.out.println(growth);
+       short amount = 22032;
+        System.out.println(amount);
+        float koe = 0.001429f;
+        System.out.println(koe);
+        int distance = 384400;
+        System.out.println(distance);
+        boolean  permits = false;
+        System.out.println(permits);
+        byte size = 80;
+        System.out.println(size);
+    }
+}
+
