172b5f0e822f8646ff3a8e06fa4796f82bb3da9d
Morgan <proger20000@gmail.com>
2022-10-22 02:37:19 +0600

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/calculate/Calculator.java
+++ src/main/java/ru/job4j/calculate/Calculator.java
@@ -10,8 +10,5 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator.plus(1, 2);
         Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
-
     }
 }
