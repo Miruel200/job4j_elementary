ad7f10a50498bb4307e7dcb7e08455ee3911574d
Morgan <proger20000@gmail.com>
2022-11-03 01:04:02 +0600

    9.3. Преобразование примитивных типов

--- src/main/java/ru/job4j/condition/Types.java
+++ src/main/java/ru/job4j/condition/Types.java
@@ -1,32 +1,30 @@
 package ru.job4j.condition;
-
 public class Types {
     public static void main(String[] args) {
          long l = 129;
          byte b = (byte) l;
-         System.out.println(b);
+         System.out.println("Произошла потеря данных " + b);
 
          float f = 12358.7f;
          int v  = 45981;
          short rsl = (short) (f + v);
-         System.out.println(rsl);
+         System.out.println("Данные в порядке " + rsl);
 
          char c = 45000;
          float t = c;
-         System.out.println(t);
+         System.out.println("Данные в порядке " + t);
 
          double d = 121.19;
          byte g = (byte) d;
-         System.out.println(g);
+         System.out.println("Произошла потеря данных " + g);
 
          short s = 1500;
          char z = (char) s;
-         System.out.println(z);
+         System.out.println("Данные в порядке " + z);
 
          short m = 1500;
          char n = (char) m;
          double h = n;
-         System.out.println(h);
-
+         System.out.println("Данные в порядке  " + h);
     }
 }
