072140855420526d0ab4b9aacf198518e4e27333
Morgan <proger20000@gmail.com>
2023-02-21 18:47:06 +0600

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -9,11 +9,11 @@ public class X2Test {
 
     @Test
     public void whenA10B0C0X2Then40() {
-        int a = 1;
+        int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expeted = 3;
+        int expeted = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expeted, rsl);
 
