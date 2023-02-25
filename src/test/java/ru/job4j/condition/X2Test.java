a2595631594c1d26c84245706190576b93c11817
Morgan <proger20000@gmail.com>
2023-02-25 06:55:25 +0600

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -18,30 +18,41 @@ public class X2Test {
         Assert.assertEquals(expeted, rsl);
 
     }
-   /* @Test
-    public void whenA10B0C0X2Then40() {
+    @Test
+    public void whenA1B0C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expeted = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expeted, rsl);
+
+    }
+     @Test
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expeted = 1;
+        int expeted = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expeted, rsl);
 
     }
     @Test
-    public void whenA10B0C0X2Then40() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        int expeted = 1;
+        int expeted = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expeted, rsl);
 
     }
     @Test
-    public void whenA10B0C0X2Then40() {
+    public void whenA1B1C1X0Then2() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -50,5 +61,5 @@ public class X2Test {
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expeted, rsl);
 
-    }*/
+    }
 }
