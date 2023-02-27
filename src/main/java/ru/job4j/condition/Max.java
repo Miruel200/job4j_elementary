6733664e85ad9377a6eea269b9abc6dcb794580a
Morgan <proger20000@gmail.com>
2023-02-27 20:19:23 +0600

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,7 +7,7 @@ public class Max {
     }
 
     public static void main(String[] args) {
-        int result = Max.max(1, 2);
+        int result = Max.max(2, 4);
         System.out.println(result);
     }
 }
