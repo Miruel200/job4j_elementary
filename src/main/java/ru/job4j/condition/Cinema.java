5a66b478a24b4a0ff10b7aa55ed65f1ba7721762
Morgan <proger20000@gmail.com>
2023-02-27 12:45:35 +0600

    4.1. Операторы ветвлений [#3611]

--- src/main/java/ru/job4j/condition/Cinema.java
+++ src/main/java/ru/job4j/condition/Cinema.java
@@ -7,9 +7,9 @@ public class Cinema {
             System.out.println("Welcome to the cinema.");
         } else {
             System.out.println("It is not for you.");
-
         }
     }
+
     public static void main(String[] args) {
         Cinema.access(21);
         Cinema.access(16);
